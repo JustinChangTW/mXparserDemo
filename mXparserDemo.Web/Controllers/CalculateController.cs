c49d286c240a9065c352be2538eeecbb293b44ac
justin <justin@arcran.com>
2020-12-15 11:47:30 +0800

    使用WebApi來執行

---
+++ mXparserDemo.Web/Controllers/CalculateController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using mXparserDemo.Service;
+using mXparserDemo.Web.Model;
+
+namespace mXparserDemo.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculateController : ControllerBase
+    {
+        private readonly CalculateService _calculateService;
+
+        public CalculateController()
+        {
+            _calculateService = new CalculateService();
+        }
+
+       public double Post([FromBody] CalculateViewModel calculateViewModel)
+        {
+            var result = _calculateService.Calculate(calculateViewModel.Formula,()=> new { calculateViewModel.para });
+            return result;
+        }
+    }
+}
---
+++ mXparserDemo.Web/Model/CalculateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mXparserDemo.Web.Model
+{
+    public class CalculateViewModel
+    {
+        public string Formula { get; set; }
+        public Parameter para { get; set; }
+    }
+
+    public class Parameter
+    {
+        public double a { get; set; }
+        public double b { get; set; }
+    }
+
+}
